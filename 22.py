beaed0981a9fe36e1048c1510be696a2395f7cb2
LiGuoshen <guoshen.li@u.nus.edu>
2022-03-20 21:21:34 +0800

    my first test

--- 22.py
+++ 22.py
@@ -25,30 +25,29 @@ class Solution:
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-
-        # 记录左右括号个数的差值  #left - #right 应该 >= 0才可以。
-
+        # 生成过程中左括号的数量 >= 右括号的数量
         res = []
-
-        def dfs(tmp, index, num_left, num_right):
-            if index > n * 2: return None
-            elif num_left < num_right: return None
-            elif num_left > n or num_right > n: return None
-            elif num_left == num_right == n:
-                res.append(''.join(tmp))
+        def dfs(index, num_left, num_right, tmp):
+            if num_left < num_right: return None
+            if index == 2 * n:
+                if num_left == n and num_right == n:
+                    res.append(''.join(tmp))
                 return None
 
-            for i in ['(', ')']:
-                tmp.append(i)
-                if i == '(':
-                    dfs(tmp, index + 1, num_left + 1, num_right)
-                elif i == ')':
-                    dfs(tmp, index + 1, num_left, num_right + 1)
 
-                tmp.pop()
 
-        dfs([], 0, 0, 0)
+            tmp.append('(')
+            dfs(index + 1, num_left + 1, num_right, tmp)
+            tmp.pop()
 
+            tmp.append(')')
+            dfs(index + 1, num_left, num_right + 1, tmp)
+            tmp.pop()
+
+
+
+        dfs(0, 0, 0, [])
         return res
+
 
 print(Solution().generateParenthesis(n = 1))
