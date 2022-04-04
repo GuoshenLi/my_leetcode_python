219d9853b0e735c795426c3190c965f947522aa7
LiGuoshen <guoshen.li@u.nus.edu>
2022-04-04 16:31:15 +0800

    2022.4.4 16:31

--- 331.py
+++ 331.py
@@ -1,28 +1,28 @@
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
-        if preorder == '#': return True
-        # 特判
-        in_degree = out_degree = 0
         preorder = preorder.split(',')
+
+        in_degree = 0
+        out_degree = 0
         n = len(preorder)
+        if preorder[0] == '#':
+            return n == 1
 
         for i in range(n):
             if i == 0:
-                if preorder[0] == '#':
-                    return False
-                # 特判
                 out_degree += 2
-                continue
-
-            if preorder[i] == '#':
-                in_degree += 1
-
             else:
-                in_degree += 1
-                out_degree += 2
 
-            if i != n - 1 and in_degree >= out_degree: return False
+                if preorder[i].isdigit():
+                    in_degree += 1
+                    out_degree += 2
+
+                elif preorder[i] == '#':
+                    in_degree += 1
+
+
+                if i != n - 1 and in_degree >= out_degree:
+                    return False
+
 
         return in_degree == out_degree
-
-
--- 652.py
+++ 652.py
@@ -23,3 +23,32 @@ class Solution:
             return serial
         dfs(root)
         return self.res
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+
+        self.table = defaultdict(list)
+
+        # key: 先序遍历 val: tree_node
+        def dfs(root):
+            if not root: return '#'
+
+            string = str(root.val) + ' ' + dfs(root.left) + ' ' + dfs(root.right)
+            self.table[string].append(root)
+
+            return string
+
+        dfs(root)
+        return list(v[0] for k, v in self.table.items() if len(v) > 1)
+
