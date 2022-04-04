e583d6ccb3a9aeeec22e0140e42ae9462f5a035b
LiGuoshen <guoshen.li@u.nus.edu>
2022-04-04 16:56:07 +0800

    2022.4.4 16:56

--- 156.py
+++ 156.py
@@ -2,11 +2,9 @@ class Solution:
     def upsideDownBinaryTree(self, root: TreeNode) -> TreeNode:
         if not root or not root.left: return root
 
-        rootLeft = root.left
-        rootRight = root.right
-        resRoot = self.upsideDownBinaryTree(rootLeft)
-        rootLeft.left = rootRight
-        rootLeft.right = root
+        resRoot = self.upsideDownBinaryTree(root.left)
+        root.left.left = root.right
+        root.left.right = root
         root.left = None
         root.right = None
         return resRoot
