00d4333e668d31f945928c6e37153cde925f5f68
LiGuoshen <guoshen.li@u.nus.edu>
2022-04-16 10:21:37 +0800

    2022.4.16 10:20

--- 26.py
+++ 26.py
@@ -1,12 +1,36 @@
+# 双指针
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[j] != nums[i]:
+                nums[j + 1] = nums[i]
+                j += 1
+
+
+        return j + 1
+
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
-        i = 0
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return len(nums)
 
-        for j in range(len(nums)):
-            if nums[j] != nums[i]:
-                nums[i + 1] = nums[j]
-                i += 1
+        j, count = 1, 1
 
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
 
-        return i + 1
+            if count <= 1:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
