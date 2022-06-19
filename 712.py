eefefd40d5280c2600fa60f12b7e8ab22b188ce8
GuoshenLi <1208298844@qq.com>
2022-06-19 15:43:15 +0800

    2022/6/19

--- 712.py
+++ 712.py
@@ -28,3 +28,22 @@ class Solution:
         return m_sum + n_sum - 2 * dfs(0, 0)
 
 
+
+
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+
+        # ASCII码最大的公共子序列
+        m = len(s1)
+        n = len(s2)
+
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + ord(s1[i - 1])
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return sum(ord(char) for char in s1) + sum(ord(char) for char in s2) - 2 * dp[-1][-1]
