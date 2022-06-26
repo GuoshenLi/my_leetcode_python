196259a2035a40f25fd0c0c543269ee2ede9fae7
GuoshenLi <1208298844@qq.com>
2022-06-26 15:19:57 +0800

    2022/6/26

--- 3.py
+++ 3.py
@@ -23,3 +23,27 @@ class Solution:
             max_len = max(max_len, right - left)
 
         return max_len
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        n = len(s)
+        table = set()
+
+        left, right = 0, 0
+        res = 0
+
+        while right < n:
+
+            while right < n and s[right] not in table:
+                table.add(s[right])
+                right += 1
+
+            res = max(res, right - left)
+
+            while right < n and s[right] in table:
+                table.remove(s[left])
+                left += 1
+
+        return res
