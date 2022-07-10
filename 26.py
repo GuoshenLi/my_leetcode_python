330155f5dc1a3bcf850582d82567d00c3bd1ee07
GuoshenLi <1208298844@qq.com>
2022-07-10 17:00:03 +0800

    2022/7/10

--- 26.py
+++ 26.py
@@ -34,3 +34,24 @@ class Solution:
                 j += 1
 
         return j
+
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+
+        i = j = 0
+
+        while j < len(nums):
+
+            if nums[i] == nums[j]:
+                j += 1
+            else:
+                i += 1
+                nums[i] = nums[j]
+                j += 1
+
+
+        return i + 1
