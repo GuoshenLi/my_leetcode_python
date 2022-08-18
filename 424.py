bd98c4eaaab5d860ad92cde55a037dd6e88f9c79
GuoshenLi <1208298844@qq.com>
2022-08-18 22:33:51 +0800

    2022/8/18

--- 424.py
+++ 424.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 # 字节跳动笔试题目
-
+# O(n ** 2)时间复杂度
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
@@ -24,5 +24,50 @@ class Solution:
             res = max(res, right - left)
         return res
 
+
+#O(n) 时间复杂度
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        num = [0] * 26
+        n = len(s)
+        res = max_number = left = right = 0
+
+        while right < n:
+            num[ord(s[right]) - ord("A")] += 1
+            max_number = max(max_number, num[ord(s[right]) - ord("A")])
+            right += 1
+
+            while right - left - max_number > k:
+                num[ord(s[left]) - ord("A")] -= 1
+                left += 1
+
+            res = max(res, right - left)
+        return res
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        window = defaultdict(int)
+        left = 0
+        right = 0
+        max_number = 0
+        res = 0
+
+        while right < len(s):
+            window[s[right]] += 1
+            max_number = max(max_number, window[s[right]])
+            right += 1
+
+            while right - left - max_number > k:
+                window[s[left]] -= 1
+                left += 1
+
+            res = max(res, right - left)
+
+        return res
 
 print(Solution().characterReplacement(s = "AABABBA", k = 1))
