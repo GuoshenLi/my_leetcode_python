2baf8264472de3a206985bab6ae580c79b2dbb5e
GuoshenLi <1208298844@qq.com>
2022-08-22 23:04:13 +0800

    2022/8/22

--- 863.py
+++ 863.py
@@ -4,6 +4,51 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+from collections import defaultdict
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+
+        parents = defaultdict(int)
+        res = []
+
+        def find_parents(root):
+            if not root: return None
+            if root.left:
+                parents[root.left.val] = root
+            if root.right:
+                parents[root.right.val] = root
+            find_parents(root.left)
+            find_parents(root.right)
+
+        def dfs(cur_node, previous_node, depth):
+            if not cur_node: return None
+
+            if depth == k:
+                res.append(cur_node.val)
+
+            if cur_node.left != previous_node:
+                dfs(cur_node.left, cur_node, depth + 1)
+
+            if cur_node.right != previous_node:
+                dfs(cur_node.right, cur_node, depth + 1)
+
+            if parents[cur_node.val] != previous_node:
+                dfs(parents[cur_node.val], cur_node, depth + 1)
+
+
+        find_parents(root)
+        dfs(target, None, 0)
+        return res
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 from collections import defaultdict, deque
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
