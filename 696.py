44f702fddaacdcb5169ea36ea7e0386a46e4f867
GuoshenLi <1208298844@qq.com>
2022-08-26 23:23:44 +0800

    2022/8/26

--- 696.py
+++ 696.py
@@ -18,3 +18,24 @@ class Solution:
         for i in range(1, len(cache)):
             res += min(cache[i - 1], cache[i])
         return res
+
+# 双指针
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        nums = []
+
+        right = 0
+        left = 0
+
+        while right < len(s):
+            while right < len(s) and s[right] == s[left]:
+                right += 1
+            nums.append(right - left)
+
+            left = right
+
+        res = 0
+        for i in range(1, len(nums)):
+            res += min(nums[i], nums[i - 1])
+
+        return res
