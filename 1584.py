3a1f61726eae72ec65825ec67517444f6a7cbe8a
GuoshenLi <1208298844@qq.com>
2022-08-27 16:55:53 +0800

    2022/8/27

--- 1584.py
+++ 1584.py
@@ -3,36 +3,81 @@ import heapq
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        #
-        in_MST = [False] * n
-        min_dis = [float('+inf')] * n
-        dis_graph = [[float('+inf')] * n for i in range(n)]
+        res = 0
+
+        adj_matrix = [[] * n for i in range(n)]
+
         for i in range(n):
             for j in range(i + 1, n):
                 w = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                dis_graph[i][j] = w
-                dis_graph[j][i] = w
+                adj_matrix[i].append((j, w))
+                adj_matrix[j].append((i, w))
 
-        # 从结点0开始
-        # 初始化
-        in_MST[0] = True
-        res = 0
-        for i in range(1, n):
-            min_dis[i] = dis_graph[0][i]
+        lowcost = [float("+inf") for i in range(n)]
+        visited = [False for i in range(n)]
+        heap = [(0, 0)]
 
-        for _ in range(1, n):
-            min_val = float('+inf')
-            for j in range(n):
-                if not in_MST[j] and min_dis[j] < min_val:
-                    min_index = j
-                    min_val = min_dis[j]
+        while heap:
+            distance, u = heapq.heappop(heap)
+            if visited[u] == True: continue
+            visited[u] = True
+            res += distance
 
-            res += min_val
-            in_MST[min_index] = True
-
-            for j in range(n):
-                if not in_MST[j] and dis_graph[j][min_index] < min_dis[j]:
-                    min_dis[j] = dis_graph[j][min_index]
-
+            for v, w in adj_matrix[u]:
+                if visited[v] == False and lowcost[v] > w:
+                    lowcost[v] = w
+                    heapq.heappush(heap, (w, v))
 
         return res
+
+# kruskal 算法 没多难。。。
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+        self.rank = [1] * n
+
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+
+        if x_root != y_root:
+            if self.rank[x_root] < self.rank[y_root]:
+                self.parents[x_root] = y_root
+            elif self.rank[x_root] > self.rank[y_root]:
+                self.parents[y_root] = x_root
+            else:
+                self.parents[y_root] = x_root
+                self.rank[y_root] += 1
+            return True
+        else:
+            return False
+
+    def find(self, x):
+        while self.parents[x] != -1:
+            x = self.parents[x]
+        return x
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        calculate_distance = lambda point1, point2: abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                edges.append([calculate_distance(points[i], points[j]), i, j])
+
+        edges.sort()
+        uf = UnionFind(n)
+        length = 0
+        count = 0
+        for i in range(len(edges)):
+
+            if uf.union(edges[i][1], edges[i][2]):
+                length += edges[i][0]
+                count += 1
+                if count == n - 1:
+                    break
+        return length
