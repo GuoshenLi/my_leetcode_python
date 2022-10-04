01a075734e63032799fcbe9102d0e2271105384c
GuoshenLi <1208298844@qq.com>
2022-10-04 22:53:08 +0800

    2022/10/4

--- 3.py
+++ 3.py
@@ -1,49 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        max_len = 0
-
-        right = 0
-        left = 0
-
-        n = len(s)
-        window = set()
-
-        while right < n:
-            if s[right] not in window:
-                window.add(s[right])
-                right += 1
-
-            else:
-                while s[right] in window:
-                    window.remove(s[left])
-                    left += 1
-
-
-            max_len = max(max_len, right - left)
-
-        return max_len
-
-
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-
-        n = len(s)
         table = set()
 
-        left, right = 0, 0
-        res = 0
+        left = right = 0
+        max_len = 0
 
-        while right < n:
-
-            while right < n and s[right] not in table:
+        while right < len(s):
+            while right < len(s) and s[right] not in table:
                 table.add(s[right])
                 right += 1
 
-            res = max(res, right - left)
+            max_len = max(max_len, right - left)
+            table.remove(s[left])
+            left += 1
 
-            while right < n and s[right] in table:
-                table.remove(s[left])
-                left += 1
 
-        return res
+        return max_len
