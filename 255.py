edef3ab8273b49ad5667a1536c862d3633d15b97
ByteDance <bytedance@MacBook-Pro.local>
2025-11-21 10:49:09 +0800

    Add some

--- 255.py
+++ 255.py
@@ -12,3 +12,33 @@ class Solution:
             stack.append(preorder[i])
 
         return True
+
+
+
+
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        if not preorder:
+            return True
+
+        root = preorder[0]
+        i = 1
+
+        while i < len(preorder) and preorder[i] < root:
+            i += 1
+
+        j = i
+        while j < len(preorder):
+            if preorder[j] < root:
+                return False
+            j += 1
+
+        left = True
+        if i > 1:
+            left = self.verifyPreorder(preorder[1: i])
+
+        right = True
+        if i < len(preorder) - 1:
+            right  =self.verifyPreorder(preorder[i: ])
+
+        return left and right
--- 剑指 Offer 33. 二叉搜索树的后序遍历序列.py
+++ 剑指 Offer 33. 二叉搜索树的后序遍历序列.py
@@ -13,15 +13,29 @@ class Solution:
 
 
 class Solution:
-    def verifyPostorder(self, postorder: [int]) -> bool:
-        def recur(i, j):
-            if i >= j: return True
-            p = i
-            while postorder[p] < postorder[j]: p += 1
-            m = p
-            while postorder[p] > postorder[j]: p += 1
-            return p == j and recur(i, m - 1) and recur(m, j - 1)
+    def verifyTreeOrder(self, postorder: List[int]) -> bool:
+        if not postorder: return True
+        root = postorder[-1]
+        i = 0
+        # 找到第一个大于根的元素的位置
+        while i < len(postorder) - 1 and postorder[i] < root:
+            i += 1
+        # 检查左侧部分是否都小于根节点
+        j = i
+        while j < len(postorder) - 1:
+            if postorder[j] < root:
+                return False
+            j += 1
+        # 递归检查左右子树
+        left = True
+        if i > 0:
+            left = self.verifyTreeOrder(postorder[:i])
 
-        return recur(0, len(postorder) - 1)
+        right = True
+
+        if i < len(postorder) - 1:
+            right = self.verifyTreeOrder(postorder[i:-1])
+
+        return left and right
 
 
