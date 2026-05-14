4f4d0f2411f1135f7b7b55d62e94b0fde6ed58bc
outtestleo <1208298844@qq.com>
2026-05-14 20:31:14 +0800

    Add: 20260514

--- 59.py
+++ 59.py
@@ -31,3 +31,37 @@ class Solution:
             left += 1
 
         return matrix
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+        total_number = n ** 2
+        count = 1
+        left, right, up, down = 0, n - 1, 0, n - 1
+        while True:
+            for j in range(left, right + 1):
+                matrix[up][j] = count
+                count += 1
+            up += 1
+            if up > down: return matrix
+            for i in range(up, down + 1):
+                matrix[i][right] = count
+                count += 1
+
+            right -= 1
+            if right < left: return matrix
+            for j in range(right, left - 1, -1):
+                matrix[down][j] = count
+                count += 1
+
+            down -= 1
+            if up > down: return matrix
+            for i in range(down, up - 1, -1):
+                matrix[i][left] = count
+                count += 1
+
+            left += 1
+            if left > right: return matrix
+
