e98f98c42c0c341f14226cf303dd7423b6c0a3e1
outtestleo <1208298844@qq.com>
2026-05-19 17:42:00 +0800

    Add: 20260519

--- 219.py
+++ 219.py
@@ -15,3 +15,26 @@ class Solution:
         return False
 
 
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        '''
+            [1, 2, 1, 4, 5, 1, 2, 3, 4]
+            维护一个长度为k的窗口　
+
+        '''
+        window = set()
+        right = 0
+        left = 0
+        while right < len(nums):
+            if right - left > k:
+                window.remove(nums[left])
+                left += 1
+
+            if nums[right] in window:
+                return True
+
+            window.add(nums[right])
+            right += 1
+        return False
+
