3826bd008f7896a33baaa3d134df3842abf9e6b9
outtestleo <1208298844@qq.com>
2026-05-27 19:39:58 +0800

    Add: 20260527

--- 485.py
+++ 485.py
@@ -11,3 +11,23 @@ class Solution:
                 count = 0
 
         return res
+
+# 20260527 双指针 直接秒了
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        res = 0
+        while right < len(nums):
+            while right < len(nums) and nums[right] == 1:
+                right += 1
+
+            res = max(res, right - left)
+
+            left = right
+
+            while left < len(nums) and nums[left] == 0:
+                left += 1
+            right = left
+
+        return res
