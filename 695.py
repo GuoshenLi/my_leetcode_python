0ce471dd4a919420fb3e2973e7727ea0ade01760
outtestleo <1208298844@qq.com>
2026-07-07 23:47:28 +0800

    Add: 20260707

--- 695.py
+++ 695.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import List
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
@@ -30,3 +31,36 @@ class Solution:
 
 
         return res
+
+
+from collections import deque
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        m = len(grid)
+        n = len(grid[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    queue = deque()
+                    queue.append((i, j))
+                    grid[i][j] = 0
+                    max_count = 0
+
+                    while queue:
+                        x, y = queue.popleft()
+                        max_count += 1
+
+                        for indent_x, indent_y in directions:
+                            new_x = x + indent_x
+                            new_y = y + indent_y
+                            if 0 <= new_x <= m - 1 and 0 <= new_y <= n - 1 and grid[new_x][new_y] == 1:
+                                queue.append((new_x, new_y))
+                                grid[new_x][new_y] = 0
+                    res = max(res, max_count)
+        return res
+
