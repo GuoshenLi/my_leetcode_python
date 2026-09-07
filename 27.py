a0d0340c54ec55f1acb0451cea31aeb3d443428d
outtestleo <1208298844@qq.com>
2026-09-07 12:11:31 +0800

    Add: 20260907

--- 27.py
+++ 27.py
@@ -2,9 +2,12 @@
 # i指向val的位置，j遍历数组看是否是val
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != val:
-                nums[i] = nums[j]
-                i += 1
-        return i
+        left, right = 0, 0
+
+        while right < len(nums):
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left += 1
+
+            right += 1
+        return left
