2756617642ff752f50c9b314cfda751907d83d97
outtestleo <1208298844@qq.com>
2026-09-14 11:34:57 +0800

    Add: 202609014

--- 33.py
+++ 33.py
@@ -26,25 +26,33 @@ class Solution:
 # 以right和left作为比较对象（活动）
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return -1
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
+
+        '''
+            [0,1,2,4,5,6,7]
+
+            [4,5,6,7,0,1,2]
+
+        '''
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+            if nums[mid] == target: return mid
             # 一定要小于等于
             # 因为要看左边有序还是右边有序，就算等于也是左边有序
-            if nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    l = mid + 1
-
+                    left = mid + 1
             else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
                 else:
-                    r = mid - 1
-
+                    right = mid - 1
         return -1
+
+
